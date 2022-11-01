34545af91bc042fb531b687172bf5a1fc8edf146
unknown <Teleinformatica II@FDBAPRCISFSD380.FormacionSena.RED>
2022-11-01 15:34:50 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+ <v-app>
+  <v-main>
+    <men></men>
+    <!-- <cal></cal>
+    <car></car>
+    <divi></divi>
+    <caro></caro> -->
+   </v-main>
+   <router-view></router-view>
+ </v-app>
+ 
+ 
+</template>
+
+<script>
+import  men from './components/MenuUno.vue';
+// import  cal from './components/CalendarioAll.vue';
+// import  car from './components/CardsAll.vue';
+// import  divi from './components/DividersAll.vue';
+// import  caro from './components/CarouAll.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    men,
+    // cal,
+    // car,
+    // divi,
+    // caro
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import compras from './pages/ComprasAll.vue'
+import ventas from './pages/VentasAll.vue'
+
+export const routes =  [
+    {path:'/ventas',component:ventas},
+    {path:'/compras',component:compras},
+]
